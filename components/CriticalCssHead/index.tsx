5374726b17f15558706f750537672db5fd3ba421
Mike Francis <mikeffrancis@gmail.com>
2020-10-15 09:19:47 +0100

    Updated _document for breaking change in next@9.5.3
    https://github.com/vercel/next.js/discussions/16784

--- components/CriticalCssHead/index.tsx
+++ components/CriticalCssHead/index.tsx
@@ -1,16 +1,18 @@
 import { Head } from 'next/document';
 import { InlineStyle } from './InlineStyle';
 
-export class CriticalCssHead extends Head {
-  getCssLinks() {
-    return this.__getInlineStyles();
-  }
+type DocumentFiles = {
+  sharedFiles: string[];
+  pageFiles: string[];
+  allFiles: string[];
+};
 
-  __getInlineStyles() {
-    const { assetPrefix, files } = this.context._documentProps;
+export class CriticalCssHead extends Head {
+  getCssLinks({ allFiles }: DocumentFiles) {
+    const { assetPrefix } = this.context._documentProps;
     const { nonce } = this.props;
     const isCss = (file: string): boolean => /\.css$/.test(file);
     const renderCss = (file: string) => <InlineStyle key={file} file={file} nonce={nonce} assetPrefix={assetPrefix} />;
-    return files && files.length > 0 ? files.filter(isCss).map(renderCss) : null;
+    return allFiles && allFiles.length > 0 ? allFiles.filter(isCss).map(renderCss) : null;
   }
 }
